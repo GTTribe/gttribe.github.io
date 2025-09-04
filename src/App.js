4c0a0f6b7c455debdd6303adf29479202d2f75e8
Adam Grossberg <adamgrossberg@gatech.edu>
2025-09-04 15:44:43 -0400

    red zone elo page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import RedZoneELO from './leaderboard/RedZoneELO';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           return null;
         }}/>
         <Route path = '/home' element={<Home/>}/>
+        <Route path='/elo' element={<RedZoneELO/>}/>
       </Routes>
     </Router>
   );
